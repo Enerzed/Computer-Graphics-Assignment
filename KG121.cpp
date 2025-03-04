8005815e88dd87eb4f4a361100b6adc761ed9dd1
Enerzed <lasker7241@gmail.com>
2025-03-04 17:31:01 +0700

    finale

--- KG121.cpp
+++ KG121.cpp
@@ -1,69 +1,385 @@
-﻿#include "KG121.h"
+﻿// KG121.cpp : This file contains the 'main' function
+#include "KG121.h"
+#include <utility>
 
-void ScanLine::ReadFile(string filename) {
+
+void IntervalScan::Init(string filename, double d, double camera_x) {
 	ifstream file(filename);
-	vector<Vector3f> vertices;
+	double xobj = 0, yobj = 0, zobj = 0;
+	int counter = 0;
 	if (file.is_open()) {
-		string line;
-		while (file.good()) {
+		while (!file.eof()) {
 			char c;
 			file >> c;
-			cout << c;
 			switch (c) {
-				case 'v':
-					double x, y, z;
-					file >> x >> y >> z;
-					Vector3f vertex(x, y, z);
-					if (vertices.size() == 0) {
-						xmin = x;
-						xmax = x;
-						ymin = y;
-						ymax = y;
-					}
-					else {
-						if (x < xmin) xmin = x;
-						if (x > xmax) xmax = x;
-						if (y < ymin) ymin = y;
-						if (y > ymax) ymax = y;
-					}
-
-					vertices.push_back(vertex);
-					n_vertices++;
-					break;
-				case 'f':
-					int vertex_index;
-					vector<int> vertex_indices;
-					vector<vector<int>> face_indices;
-					for ()
-					break;
-
+			case 'v':
+				double x, y, z;
+				file >> x >> y >> z;
+				if (VT.size() == 0) {
+					xmax = x;
+					xmin = x;
+					ymax = y;
+					ymin = y;
+					zmax = z;
+					zmin = z;
+				}
+				else {
+					if (x > xmax) xmax = x;
+					if (x < xmin) xmin = x;
+					if (y > ymax) ymax = y;
+					if (y < ymin) ymin = y;
+					if (z > zmax) zmax = z;
+					if (z < zmin) zmin = z;
+				}
+				xobj += x;
+				yobj += y;
+				zobj += z;
+				VT.push_back(Vector3f(x, y, z));
+				n_verticies++;
+				break;
+			case 'f':
+				Face face;
+				Indicies indicies;
+				int vertex_index;
+				face.id = n_faces;
+				while (file.peek() != '\n' && file.peek() != EOF)
+				{
+					file >> vertex_index;
+					indicies.indicies.push_back(vertex_index - 1);
+				}
+				face.verticies.push_back(indicies);
+				FT.push_back(face);
+				n_faces++;
+				break;
 			}
+		}
+		file.close();
+		double w = WINDOW_HEIGHT * 0.6 / (ymax - ymin);
+		xobj /= n_verticies;
+		yobj /= n_verticies;
+		zobj /= n_verticies;
+		xmax = -1e9;
+		xmin = 1e9;
+		ymax = -1e9;
+		ymin = 1e9;
+		zmax = -1e9;
+		zmin = 1e9;
+		for (int i = 0; i < n_verticies; i++) {
+			VT[i].x = (VT[i].x - xobj) * w + WINDOW_HEIGHT / 2;
+			VT[i].y = (VT[i].y - yobj) * w + WINDOW_HEIGHT / 2;
+			VT[i].z = (VT[i].z - zobj) * w + WINDOW_HEIGHT / 2;
+			if (VT[i].x > xmax) xmax = VT[i].x;
+			if (VT[i].x < xmin) xmin = VT[i].x;
+			if (VT[i].y > ymax) ymax = VT[i].y;
+			if (VT[i].y < ymin) ymin = VT[i].y;
+			if (VT[i].z > zmax) zmax = VT[i].z;
+			if (VT[i].z < zmin) zmin = VT[i].z;
+		}
+		this->d = d;
+		this->camera_x = camera_x;
+		is_init = true;
+
+		// Apply perspective projection
+		for (int i = 0; i < n_verticies; i++) {
+			// Perspective projection calculations
+			VT[i].x = (VT[i].x  + camera_x) * d / (d + VT[i].z);
+			VT[i].y = VT[i].y * d / (d + VT[i].z);
+
+
+			if (VT[i].x > xmax) xmax = VT[i].x;
+			if (VT[i].x < xmin) xmin = VT[i].x;
+			if (VT[i].y > ymax) ymax = VT[i].y;
+			if (VT[i].y < ymin) ymin = VT[i].y;
+			if (VT[i].z > zmax) zmax = VT[i].z;
+			if (VT[i].z < zmin) zmin = VT[i].z;
+
+		}
+		// build edges and polygons
+		for (int i = 0; i < n_faces; i++) {
+			// build edges
+			for (int j = 0; j < FT[i].verticies[0].indicies.size(); j++) {
+				int current = FT[i].verticies[0].indicies[j];
+				int next;
+				if (j != FT[i].verticies[0].indicies.size() - 1)
+					next = FT[i].verticies[0].indicies[j + 1];
+				else
+					next = FT[i].verticies[0].indicies[0];
+				Edge edge;
+				edge.id = i;
+				//cout << edge.id << endl;
+				if (VT[current].y != VT[next].y)
+					edge.dx = (VT[current].x - VT[next].x) / (VT[current].y - VT[next].y);
+				else
+					edge.dx = 0;
+				if (VT[current].y > VT[next].y) {
+					edge.ymax = VT[current].y;
+					edge.ymin = VT[next].y;
+					edge.x = VT[current].x;
+				}
+				else {
+					edge.ymax = VT[next].y;
+					edge.ymin = VT[current].y;
+					edge.x = VT[next].x;
+				}
+				ET.push_back(edge);
+			}
+			// build polygons
+			Poly* polygon = new Poly;
+			polygon->id = i;
+			polygon->is_in = false;
+			Vec4 abcd = SolveABCD(VT, FT[i].verticies[0]);
+			polygon->a = abcd.x;
+			polygon->b = abcd.y;
+			polygon->c = abcd.z;
+			polygon->d = abcd.w;
+			polygon->indicies_.push_back(FT[i].verticies[0].indicies[0]);
+			polygon->indicies_.push_back(FT[i].verticies[0].indicies[1]);
+			polygon->indicies_.push_back(FT[i].verticies[0].indicies[2]);
+			polygon->verticies.push_back(VT[FT[i].verticies[0].indicies[0]]);
+			polygon->verticies.push_back(VT[FT[i].verticies[0].indicies[1]]);
+			polygon->verticies.push_back(VT[FT[i].verticies[0].indicies[2]]);
+			//cout << polygon->verticies[0].x << " " << polygon->verticies[0].y << " " << polygon->verticies[0].z << endl;
+			//cout << polygon->verticies[1].x << " " << polygon->verticies[1].y << " " << polygon->verticies[1].z << endl;
+			//cout << polygon->verticies[2].x << " " << polygon->verticies[2].y << " " << polygon->verticies[2].z << endl;
+			polygon->CalculateNormal();
+			//cout << polygon.normal.x << " " << polygon.normal.y << " " << polygon.normal.z << endl;
+			polygon->color = Color::White;
+			PT.push_back(*polygon);
+		}
+		// По умолчанию нормали к вершинам равны нормали полигона
+		for (auto it = PT.begin(); it != PT.end(); it++) {
+			it->normals_.push_back(it->normal);
+			it->normals_.push_back(it->normal);
+			it->normals_.push_back(it->normal);
+		}
+		// Поиск соседних полигонов и усреднение их нормалей
+		for (auto it = PT.begin(); it != PT.end(); it++)
+			for (auto it2 = PT.begin(); it2 != PT.end(); it2++) {
+				if (it->indicies_[0] == it2->indicies_[0]) {
+					it->normals_[0] = Normalize((it->normals_[0] + it2->normals_[0]));
+					it2->normals_[0] = Normalize((it->normals_[0] + it2->normals_[0]));
+				}
+				if (it->indicies_[1] == it2->indicies_[0]) {
+					it->normals_[1] = Normalize((it->normals_[1] + it2->normals_[0]));
+					it2->normals_[0] = Normalize((it->normals_[1] + it2->normals_[0]));
+				}
+				if (it->indicies_[2] == it2->indicies_[0]) {
+					it->normals_[2] = Normalize((it->normals_[2] + it2->normals_[0]));
+					it2->normals_[0] = Normalize((it->normals_[2] + it2->normals_[0]));
+				}
+				if (it->indicies_[0] == it2->indicies_[1]) {
+					it->normals_[0] = Normalize((it->normals_[0] + it2->normals_[1]));
+					it2->normals_[1] = Normalize((it->normals_[0] + it2->normals_[1]));
+				}
+				if (it->indicies_[1] == it2->indicies_[1]) {
+					it->normals_[1] = Normalize((it->normals_[1] + it2->normals_[1]));
+					it2->normals_[1] = Normalize((it->normals_[1] + it2->normals_[1]));
+				}
+				if (it->indicies_[2] == it2->indicies_[1]) {
+					it->normals_[2] = Normalize((it->normals_[2] + it2->normals_[1]));
+					it2->normals_[1] = Normalize((it->normals_[2] + it2->normals_[1]));
+				}
+				if (it->indicies_[0] == it2->indicies_[2]) {
+					it->normals_[0] = Normalize((it->normals_[0] + it2->normals_[2]));
+					it2->normals_[2] = Normalize((it->normals_[0] + it2->normals_[2]));
+				}
+				if (it->indicies_[1] == it2->indicies_[2]) {
+					it->normals_[1] = Normalize((it->normals_[1] + it2->normals_[2]));
+					it2->normals_[2] = Normalize((it->normals_[1] + it2->normals_[2]));
+				}
+				if (it->indicies_[2] == it2->indicies_[2]) {
+					it->normals_[2] = Normalize((it->normals_[2] + it2->normals_[2]));
+					it2->normals_[2] = Normalize((it->normals_[2] + it2->normals_[2]));
+				}
+			}
+
+
+		// Background
+		{
+			Poly background;
+			background.id = PT.size() - 1;
+			background.is_in = false;
+			background.a = 0;
+			background.b = 0;
+			background.c = -1e12;
+			background.d = WINDOW_HEIGHT;
+			background.color = Color::Black;
+			background.indicies_.push_back(-1);
+			background.indicies_.push_back(-1);
+			background.indicies_.push_back(-1);
+			background.verticies.push_back(Vector3f(0, 0, -1e12));
+			background.verticies.push_back(Vector3f(0, WINDOW_HEIGHT, -1e12));
+			background.verticies.push_back(Vector3f(WINDOW_WIDTH, WINDOW_HEIGHT, -1e12));
+			background.normal = Vector3f(0, 0, 0);
+			background.normals_.push_back(background.normal);
+			background.normals_.push_back(background.normal);
+			background.normals_.push_back(background.normal);
+			PT.push_back(background);
+
+			Edge background_edge1;
+			background_edge1.id = PT.size() - 1;
+			background_edge1.dx = 0;
+			background_edge1.ymax = WINDOW_HEIGHT - 1;
+			background_edge1.ymin = 0;
+			background_edge1.x = 0;
+			ET.push_back(background_edge1);
+
+			Edge background_edge2;
+			background_edge2.id = PT.size() - 1;
+			background_edge2.dx = 0;
+			background_edge2.ymax = WINDOW_HEIGHT - 1;
+			background_edge2.ymin = 0;
+			background_edge2.x = WINDOW_WIDTH;
+			ET.push_back(background_edge2);
+
+
+			Poly background2;
+			background2.id = PT.size() - 1;
+			background2.is_in = false;
+			background2.a = 0;
+			background2.b = 0;
+			background2.c = -1e12;
+			background2.d = WINDOW_HEIGHT;
+			background2.color = Color::Black;
+			background2.indicies_.push_back(-1);
+			background2.indicies_.push_back(-1);
+			background2.indicies_.push_back(-1);
+			background2.verticies.push_back(Vector3f(0, 0, -1e12));
+			background2.verticies.push_back(Vector3f(WINDOW_WIDTH, 0, -1e12));
+			background2.verticies.push_back(Vector3f(WINDOW_WIDTH, WINDOW_HEIGHT, -1e12));
+			background2.normal = Vector3f(0, 0, 0);
+			background2.normals_.push_back(background2.normal);
+			background2.normals_.push_back(background2.normal);
+			background2.normals_.push_back(background2.normal);
+			PT.push_back(background2);
+
+			Edge background_edge12;
+			background_edge12.id = PT.size() - 1;
+			background_edge12.dx = 0;
+			background_edge12.ymax = WINDOW_HEIGHT - 1;
+			background_edge12.ymin = 0;
+			background_edge12.x = 0;
+			ET.push_back(background_edge12);
+
+			Edge background_edge22;
+			background_edge22.id = PT.size() - 1;
+			background_edge22.dx = 0;
+			background_edge22.ymax = WINDOW_HEIGHT - 1;
+			background_edge22.ymin = 0;
+			background_edge22.x = WINDOW_WIDTH;
+			ET.push_back(background_edge22);
 		}
 	}
 	else {
-		cout << "Could not open file" << endl;
+		cout << "Unable to open file";
 		exit(1);
 	}
 }
 
+Image IntervalScan::Scan(bool is_black_white, Light light, bool is_flat) {
+
+	Image image;
+	image.create(WINDOW_WIDTH, WINDOW_HEIGHT, Color::Black);
+
+	for (int y = WINDOW_HEIGHT - 1; y >= 0; y--) {
+		// Add edges to AET
+		for (int i = 0; i < ET.size(); i++) {
+			if (ET[i].ymax >= ET[i].ymin && ET[i].ymax == y) {
+				if (ET[i].ymax > ET[i].ymin)
+					AET.push_back(ET[i]);
+				ET[i].ymax--;
+				ET[i].x -= ET[i].dx;
+			}
+		}
+		// Sort AET by x
+		sort(AET.begin(), AET.end(), [](const Edge& a, const Edge& b) { return a.x < b.x; });
+		if (AET.empty()) continue;
+		for (size_t i = 0; i < AET.size(); i++) {
+			int current_id = AET[i].id;
+			// Calculate lightning
+
+			PT[current_id].is_in = !PT[current_id].is_in;
+			if (PT[current_id].is_in)
+				APT.push_back(PT[current_id]);
+			else {
+				auto it = std::remove_if(APT.begin(), APT.end(), [&](const Poly& poly) { return poly.id == current_id; });
+				APT.erase(it, APT.end());
+			}
 
 
+			if (i + 1 < AET.size()) {
+				double x_start = AET[i].x;
+				double x_end = AET[i + 1].x;
 
+				for (int x = static_cast<int>(x_start); x < static_cast<int>(x_end); ++x) {
+					double zmax = -INFINITY;
+					int show_id = -1;
+					for (auto& poly : APT) {
+						double z = poly.GetZ(x, y);
+						if (z > zmax) {
+							zmax = z;
+							show_id = poly.id;
+						}
+					}
+					std::vector<Color> vertexColor;
+					for (int v = 0; v < 3; v++)
+					{
+
+						//cout << PT[show_id].verticies[v].x << " " << PT[show_id].verticies[v].y << " " << PT[show_id].verticies[v].z << endl;
+						if (is_flat)
+							vertexColor.push_back(CalculateLightning(PT[show_id].verticies[v], PT[show_id].normal, light));
+						else
+							vertexColor.push_back(CalculateLightning(PT[show_id].verticies[v], PT[show_id].normals_[v], light));
+						//if ((int)vertexColor[v].r != 0 || (int)vertexColor[v].g != 0 || (int)vertexColor[v].b != 0)
+						//	std::cout << (int)vertexColor[v].r << " " << (int)vertexColor[v].g << " " << (int)vertexColor[v].b << std::endl;
+					}
+					if (show_id != PT.size() - 1 && show_id != PT.size() - 2) {
+
+						Color interpolatedColor = BarycentricInterpolation(PT[show_id], vertexColor, x, y);
+
+						image.setPixel(x, WINDOW_HEIGHT - y - 1, interpolatedColor);
+					}
+				}
+			}
+		}
+		AET.clear();
+	}
+	cout << "Done" << endl;
+	return image;
+}
 
 int main() {
-	RenderWindow window(VideoMode(1000, 1000), "KG121", Style::Close);
-	ScanLine scan_line;
-	scan_line.ReadFile("polygons.obj");
-
+	Light light;
+	std::string filename = "cylinder.obj";
+	double initial = -6000.f;
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "LAB 3");
+	IntervalScan intervalScan;
+	intervalScan.Init(filename, initial, 0.f);
 	while (window.isOpen()) {
 		Event event;
+		IntervalScan copy;
+
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed)
 				window.close();
+			if (event.type == Event::KeyPressed) {
+				if (event.key.code == Keyboard::Left)
+					copy.Init(filename, initial, intervalScan.camera_x - 100);
+				if (event.key.code == Keyboard::Right)
+					copy.Init(filename, initial, intervalScan.camera_x + 100);
+			}
 		}
+		if (copy.is_init == false)
+			copy = intervalScan;
+		Image image = intervalScan.Scan(true, light, false);
+		intervalScan = copy;
 
+		Sprite sprite;
+		Texture texture;
+		texture.loadFromImage(image);
+		sprite.setTexture(texture);
 		window.clear();
-
+		window.draw(sprite);
 		window.display();
 	}
 }
--- KG121.h
+++ KG121.h
@@ -1,46 +1,179 @@
 ﻿#pragma once
+// KG121.h
+/*
+    Эта программа заняла у меня более 100 часов...
+*/
+
 
 #include <iostream>
 #include <vector>
-#include <list>
 #include <algorithm>
 #include <fstream>
+#include <cmath>
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+#define WINDOW_WIDTH 1000
+#define WINDOW_HEIGHT 1000
+
 
 using namespace std;
 using namespace sf;
+using namespace Glsl;
+
+
+double DotProduct(Vector3f v1, Vector3f v2) {
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+Vector3f CrossProduct(Vector3f v1, Vector3f v2) {
+	return Vector3f(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
+
+Vector3f Normalize(Vector3f v) {
+	float length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+	return Vector3f(v.x / length, v.y / length, v.z / length);
+}
+
+double s2(double x) { return x * x; }
+
+struct Light {
+    Vector3f position;
+    Color intensity;
+    Light() {
+        position = Vector3f(1000, 2000, -5000);
+        intensity = Color::White;
+    }
+};
+
+class Indicies {
+public:
+    vector<int> indicies;
+};
+
+class Face {
+public:
+    int id;
+    vector<Indicies> verticies;
+};
 
 class Edge {
 public:
-	int id, dy;
-	double x, dx;
+    int id, dy, ymax, ymin;
+    double x, dx, current_x;
 };
 
-class Polygon {
+class Poly {
 public:
-	int id;
-	Glsl::Vec4 abcd;
-	double dy;
-	bool is_in = false;
-	Color color;
+    int id;
+    double a, b, c, d;
+    double dy;
+    Vector3f normal;
+    bool is_in;
+    vector<Vector3f> verticies;
+    vector<Vector3f> normals_;
+    vector<int> indicies_;
+    Color color = Color::White;
+    Color light;
+
+    // Вычисление z
+    double GetZ(double x, double y) {
+        return -(a * x + b * y + d) / c;
+    }
+
+    void SetColor(Color color) {
+        this->color = color;
+    }
+    
+    // Вычисление нормали к полигону
+    void CalculateNormal() {
+        double vx1 = verticies[0].x - verticies[1].x;
+		double vy1 = verticies[0].y - verticies[1].y;
+        double vz1 = verticies[0].z - verticies[1].z;
+        double vx2 = verticies[1].x - verticies[2].x;
+		double vy2 = verticies[1].y - verticies[2].y;
+		double vz2 = verticies[1].z - verticies[2].z;
+
+        double nx = vy1 * vz2 - vz1 * vy2;
+		double ny = vz1 * vx2 - vx1 * vz2;
+		double nz = vx1 * vy2 - vy1 * vx2;
+
+        double wrki = sqrt(s2(vy1*vz2 - vz1*vy2) + s2(vz1*vx2 - vx1*vz2) + s2(vx1*vy2 - vy1*vx2));
+        nx /= wrki;
+		ny /= wrki;
+        nz /= wrki;
+        normal = Vector3f(nx, ny, nz);
+    }
 };
 
-class Interval {
+class IntervalScan {
 public:
-	int id = -1;
-	int xl, xr;
-};
+    bool is_init = false;
+    vector<Edge> ET;
+    vector<Edge> AET;
+    vector<Poly> PT;
+    vector<Poly> APT;
+    vector<Face> FT;
+    vector<Vector3f> VT;
+    int n_edges = 0, n_polygons = 0, n_faces = 0, n_verticies = 0;
+    int xmin, xmax, ymin, ymax, zmin, zmax;
+    double d = 0;
+    double camera_x = 0;
 
-class ScanLine {
-public:
-	vector<Edge> ET;
-	vector<Polygon> PT;
-	vector<Interval> IT;
-	vector<Vector3f> vertices;
-	int n_vertices = 0;
-	double ymin, ymax, xmin, xmax;
+    void Init(string filename, double d, double camera_x);
 
-    void Scan();
-	void ReadFile(string filename);
+    Image Scan(bool is_black_white, Light light, bool is_flat);
 
+    Vec4 SolveABCD(vector<Vector3f> verticies, Indicies indicies) {
+        Vec4 p;
+        p.x = (verticies[indicies.indicies[1]].y - verticies[indicies.indicies[0]].y) * (verticies[indicies.indicies[2]].z - verticies[indicies.indicies[0]].z) -
+            (verticies[indicies.indicies[1]].z - verticies[indicies.indicies[0]].z) * (verticies[indicies.indicies[2]].y - verticies[indicies.indicies[0]].y);
+        p.y = (verticies[indicies.indicies[1]].z - verticies[indicies.indicies[0]].z) * (verticies[indicies.indicies[2]].x - verticies[indicies.indicies[0]].x) -
+            (verticies[indicies.indicies[1]].x - verticies[indicies.indicies[0]].x) * (verticies[indicies.indicies[2]].z - verticies[indicies.indicies[0]].z);
+        p.z = (verticies[indicies.indicies[1]].x - verticies[indicies.indicies[0]].x) * (verticies[indicies.indicies[2]].y - verticies[indicies.indicies[0]].y) -
+            (verticies[indicies.indicies[1]].y - verticies[indicies.indicies[0]].y) * (verticies[indicies.indicies[2]].x - verticies[indicies.indicies[0]].x);
+        p.w = -(p.x * verticies[indicies.indicies[0]].x + p.y * verticies[indicies.indicies[0]].y + p.z * verticies[indicies.indicies[0]].z);
+        return p;
+    }
+
+    Poly GetPolygonByID(int id, vector<Poly> polygons) {
+        for (int i = 0; i < polygons.size(); i++)
+            if (polygons[i].id == id)
+                return polygons[i];
+    }
+
+    // Старая функция для получения цвета на основе z координаты
+    Color GetColor(Color color, double z, double zmin, double zmax, bool is_black_white) {
+        double factor = (z - zmin) / (zmax - zmin);
+        if (is_black_white)
+            return Color(factor * 255, factor * 255, factor * 255);
+        return Color(color.r * factor, color.g * factor, color.b * factor);
+
+    }
+
+    Color CalculateLightning(Vector3f vertex, Vector3f normal, Light light) 
+    {
+        Vector3f lightDir = (light.position - vertex);
+        float dotProduct = abs(DotProduct(normal, Normalize(lightDir)));
+        return Color
+        (
+            static_cast<int>(light.intensity.r * dotProduct),
+            static_cast<int>(light.intensity.g * dotProduct),
+            static_cast<int>(light.intensity.b * dotProduct)
+        );
+    }
+
+    Color BarycentricInterpolation(const Poly& poly, std::vector<Color> colors, double x, double y) {
+        if (poly.indicies_[0] == -1 || poly.indicies_[1] == -1 || poly.indicies_[2] == -1) return Color::Black;
+        double area = (poly.verticies[1].y - poly.verticies[2].y) * (poly.verticies[0].x - poly.verticies[2].x) + (poly.verticies[2].x - poly.verticies[1].x) * (poly.verticies[0].y - poly.verticies[2].y);
+        double w1 = ((poly.verticies[1].y - poly.verticies[2].y) * (x - poly.verticies[2].x) + (poly.verticies[2].x - poly.verticies[1].x) * (y - poly.verticies[2].y)) / area;
+        double w2 = ((poly.verticies[2].y - poly.verticies[0].y) * (x - poly.verticies[2].x) + (poly.verticies[0].x - poly.verticies[2].x) * (y - poly.verticies[2].y)) / area;
+        double w3 = 1 - w1 - w2;
+
+        double r = (w1 * colors[0].r + w2 * colors[1].r + w3 * colors[2].r) / (w1 + w2 + w3);
+		double g = (w1 * colors[0].g + w2 * colors[1].g + w3 * colors[2].g) / (w1 + w2 + w3);
+		double b = (w1 * colors[0].b + w2 * colors[1].b + w3 * colors[2].b) / (w1 + w2 + w3);
+
+        return Color(static_cast<unsigned char>(r), static_cast<unsigned char>(g), static_cast<unsigned char>(b));
+    }
 };
